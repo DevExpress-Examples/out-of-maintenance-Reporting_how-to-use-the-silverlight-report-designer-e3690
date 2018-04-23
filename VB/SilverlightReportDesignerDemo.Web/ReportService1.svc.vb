1074cf6a97cee831866804ed49bf3221aabd86da
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SilverlightReportDesignerDemo.Web/ReportService1.svc.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Web
+Imports DevExpress.Data.Utils.ServiceModel
+Imports DevExpress.XtraReports.Service
+Imports DevExpress.XtraReports.UI
+Imports SilverlightReportDesignerDemo.Web.DataSet1TableAdapters
+
+Namespace SilverlightReportDesignerDemo.Web
+	' NOTE: You can use the "Rename" command on the "Refactor" menu 
+	' to change the class name "ReportService1" in code, svc and config file together.
+	<SilverlightFaultBehavior> _
+	Public Class ReportService1
+		Inherits ReportService
+
+		' Register all datasources used by your reports using the following method.
+		Protected Overrides Sub RegisterDataSources(ByVal report As XtraReport, ByVal reportName As String)
+			Dim report1 As XtraReport1 = TryCast(report, XtraReport1)
+			If report1 IsNot Nothing Then
+				report1.RegisterDataSourceName("XtraReport1Data", report1.DataSource)
+			End If
+		End Sub
+
+		' Populate your datasources for an editing session using the following method.
+		Protected Overrides Sub FillDataSources(ByVal report As XtraReport, ByVal reportName As String, ByVal isDesignActive As Boolean)
+			Dim dataSet As DataSet1 = CType(report.GetDataSourceByName("XtraReport1Data"), DataSet1)
+
+			Using categoriesTableAdapter = New CategoriesTableAdapter()
+			Using productsTableAdapter = New ProductsTableAdapter()
+				categoriesTableAdapter.Fill(dataSet.Categories)
+				productsTableAdapter.Fill(dataSet.Products)
+			End Using
+			End Using
+		End Sub
+
+		' Override the following two methods to get access to a session.
+		' It is not required to override these methods when working with a file or database.
+		' In addition, make sure that in the web.config file's <system.serviceModel> section 
+		' a <serviceHostingEnvironment> parameter is added with the aspNetCompatibilityEnabled attribute enabled.
+		Protected Overrides Sub SaveReportLayout(ByVal reportName As String, ByVal layoutData() As Byte)
+			HttpContext.Current.Session(reportName) = layoutData
+		End Sub
+
+		Protected Overrides Function LoadReportLayout(ByVal reportName As String) As Byte()
+			Return CType(HttpContext.Current.Session(reportName), Byte())
+		End Function
+	End Class
+End Namespace
+
+
+
---
+++ VB/SilverlightReportDesignerDemo.Web/XtraReport1.Designer.vb
@@ -0,0 +1,139 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace SilverlightReportDesignerDemo.Web
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.dataSet11 = New SilverlightReportDesignerDemo.Web.DataSet1()
+			Me.categoriesTableAdapter = New SilverlightReportDesignerDemo.Web.DataSet1TableAdapters.CategoriesTableAdapter()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.DetailReport = New DevExpress.XtraReports.UI.DetailReportBand()
+			Me.productsTableAdapter = New SilverlightReportDesignerDemo.Web.DataSet1TableAdapters.ProductsTableAdapter()
+			Me.Detail1 = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel2 = New DevExpress.XtraReports.UI.XRLabel()
+			CType(Me.dataSet11, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1})
+			Me.Detail.HeightF = 44.79167F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.HeightF = 48F
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.HeightF = 48.95833F
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' dataSet11
+			' 
+			Me.dataSet11.DataSetName = "DataSet1"
+			Me.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' categoriesTableAdapter
+			' 
+			Me.categoriesTableAdapter.ClearBeforeFill = True
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoryName")})
+			Me.xrLabel1.Font = New System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold)
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(10.00001F, 10.00001F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(284.7916F, 23F)
+			Me.xrLabel1.StylePriority.UseFont = False
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' DetailReport
+			' 
+			Me.DetailReport.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail1})
+			Me.DetailReport.DataAdapter = Me.productsTableAdapter
+			Me.DetailReport.DataMember = "Categories.CategoriesProducts"
+			Me.DetailReport.DataSource = Me.dataSet11
+			Me.DetailReport.Level = 0
+			Me.DetailReport.Name = "DetailReport"
+			' 
+			' productsTableAdapter
+			' 
+			Me.productsTableAdapter.ClearBeforeFill = True
+			' 
+			' Detail1
+			' 
+			Me.Detail1.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel2})
+			Me.Detail1.HeightF = 23F
+			Me.Detail1.Name = "Detail1"
+			' 
+			' xrLabel2
+			' 
+			Me.xrLabel2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Categories.CategoriesProducts.ProductName")})
+			Me.xrLabel2.Font = New System.Drawing.Font("Tahoma", 9.75F)
+			Me.xrLabel2.LocationFloat = New DevExpress.Utils.PointFloat(26.04164F, 0F)
+			Me.xrLabel2.Name = "xrLabel2"
+			Me.xrLabel2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F)
+			Me.xrLabel2.SizeF = New System.Drawing.SizeF(268.75F, 23F)
+			Me.xrLabel2.StylePriority.UseFont = False
+			Me.xrLabel2.Text = "xrLabel2"
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin, Me.DetailReport})
+			Me.DataAdapter = Me.categoriesTableAdapter
+			Me.DataMember = "Categories"
+			Me.DataSource = Me.dataSet11
+			Me.Margins = New System.Drawing.Printing.Margins(49, 100, 48, 49)
+			Me.Version = "11.2"
+			CType(Me.dataSet11, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private dataSet11 As DataSet1
+		Private categoriesTableAdapter As DataSet1TableAdapters.CategoriesTableAdapter
+		Private DetailReport As DevExpress.XtraReports.UI.DetailReportBand
+		Private Detail1 As DevExpress.XtraReports.UI.DetailBand
+		Private xrLabel2 As DevExpress.XtraReports.UI.XRLabel
+		Private productsTableAdapter As DataSet1TableAdapters.ProductsTableAdapter
+	End Class
+End Namespace
