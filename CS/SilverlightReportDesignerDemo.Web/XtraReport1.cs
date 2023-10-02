f6ad6a6941552880f4e394dd89ee74fec0bd8569
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 14:16:33 +0400

    Change example status to out of maintenance

--- CS/SilverlightReportDesignerDemo.Web/XtraReport1.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace SilverlightReportDesignerDemo.Web {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-
-    }
-}
